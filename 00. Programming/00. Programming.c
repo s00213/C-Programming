7cdfb5064b76f4148234209c2a3a44ce5788a7a7
KANG SOO IN <ks00in213@gmail.com>
2024-02-23 15:31:41 +0900

    초기 커밋.

---
+++ 00. Programming/00. Programming.c
@@ -0,0 +1,12 @@
+/* C 프로그래밍 기초 */
+
+// C 프로그래밍의 구조
+#include <stdio.h>
+int main()
+{
+	printf("*****\n\n@@@@@\n@@@@@\n@@@@@\n\n@@@@@\n@   @\n@   @\n@@@@@\n\n   $   \n  $$$  \n $$$$$ \n$$$$$$$\n\nBBBBBB \nB     B\nB     B\nBBBBBB \nB     B\nB     B\nBBBBBB \n");
+	
+	return 0;
+}
+
+// \n : 줄바꿈 문자
